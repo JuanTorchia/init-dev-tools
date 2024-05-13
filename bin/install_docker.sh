2720a3baf3ff4eb86b338ac7ea08bbb8fd2de670
Juan Torchia <j.s.torchia@gmail.com>
2024-05-13 14:46:23 -0300

    [Bugfix] check docker before install

--- bin/install_docker.sh
+++ bin/install_docker.sh
@@ -2,40 +2,33 @@
 
 set -eo pipefail
 
-# Function to install required packages and add Docker's official GPG key
 setup_docker_repository() {
-    echo "Updating package information..."
-    sudo apt-get update
-    sudo apt-get install -y ca-certificates curl gnupg lsb-release
-
     echo "Adding Docker's GPG key..."
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-    echo "Setting up the repository..."
+    echo "Setting up the Docker repository..."
     echo \
       "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
       $(lsb_release -cs) stable" | \
       sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 }
 
-# Function to install Docker Engine, CLI, and Compose
 install_docker() {
-    echo "Installing Docker Engine, CLI, and Compose..."
-    sudo apt-get update
-    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-    echo "Verifying Docker installation by running the hello-world image..."
-    sudo docker run hello-world
-
-    # Save the installed Docker version to a file
-    docker_version=$(docker --version)
-    echo "$docker_version" > /tmp/docker_installed_version.txt
-    echo "Docker version $docker_version has been installed and recorded."
+    if ! command -v docker >/dev/null; then
+        echo "Installing Docker..."
+        sudo apt-get update
+        sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+        echo "Verifying Docker installation..."
+        sudo docker run hello-world
+    else
+        echo "Docker is already installed."
+    fi
 }
 
-# Main function to control the flow
 main() {
+    sudo apt-get update
+    sudo apt-get install -y ca-certificates curl gnupg lsb-release
     setup_docker_repository
     install_docker
     echo "Docker installation completed successfully."
--- bin/install_sdkman_and_java.sh
+++ bin/install_sdkman_and_java.sh
@@ -2,21 +2,36 @@
 
 set -eo pipefail
 
-echo "Installing SDKMAN..."
-curl -s "https://get.sdkman.io" | bash
+echo "Checking required tools..."
+if ! command -v unzip >/dev/null; then
+    echo "Unzip not found. Please install unzip using your package manager."
+    exit 1
+fi
 
-# Initializing SDKMAN in the current shell script
-source "$HOME/.sdkman/bin/sdkman-init.sh"
+echo "Installing SDKMAN..."
+if [[ -d "$HOME/.sdkman" ]]; then
+    echo "SDKMAN is already installed."
+else
+    curl -s "https://get.sdkman.io" | bash
+    source "$HOME/.sdkman/bin/sdkman-init.sh"
+fi
 
 # Installing specific Java versions using SDKMAN
 java_versions=("11.0.20-amzn" "17.0.8-amzn" "21.0.1-amzn" "8.0.372-amzn")
 for version in "${java_versions[@]}"; do
-    echo "Installing Java $version..."
-    sdk install java "$version"
+    if sdk list java | grep -q "$version"; then
+        echo "Java $version is already installed."
+    else
+        echo "Installing Java $version..."
+        sdk install java "$version"
+    fi
 done
 
-# Install Maven with SDKMAN
 echo "Installing Maven..."
-sdk install maven
+if sdk list maven | grep -q "Not installed"; then
+    sdk install maven
+else
+    echo "Maven is already installed."
+fi
 
 echo "SDKMAN, Java, and Maven installation completed."
